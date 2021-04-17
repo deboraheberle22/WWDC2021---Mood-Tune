7dedd458aff5218931f929bb2d461ad6f6ccd2c2
deboraheberle22 <82190800+deboraheberle22@users.noreply.github.com>
2021-04-17 11:17:19 -0300

    Add files via upload

--- WWDC2021.playground/Contents.swift
+++ WWDC2021.playground/Contents.swift
@@ -1,17 +1,13 @@
-//: A SpriteKit based Playground
 import PlaygroundSupport
 import SpriteKit
-
-// Load the SKScene from 'GameScene.sks'
 let sceneView = SKView(frame: CGRect(x:0 , y:0, width: 900, height: 1000))
-
 if let scene = IntroScene(fileNamed: "IntroScene") {
-    // Set the scale mode to scale to fit the window
-    scene.scaleMode = .aspectFit
+    scene.scaleMode = .aspectFill
+
     
-    // Present the scene
     sceneView.presentScene(scene)
 
 PlaygroundSupport.PlaygroundPage.current.liveView = sceneView
 }
- 
+
+
--- WWDC2021.playground/Sources/GameScene.swift
+++ WWDC2021.playground/Sources/GameScene.swift
@@ -15,15 +15,34 @@ public class GameScene: SKScene {
     var yellowHexagon: SKSpriteNode!
     var yellowSquare: SKSpriteNode!
     
+    var initialBlueCircleArea : SKSpriteNode!
+    var initialBlueHexagonArea : SKSpriteNode!
+    var initialBlueSquareArea : SKSpriteNode!
+    var initialOrangeCircleArea : SKSpriteNode!
+    var initialOrangeSquareArea : SKSpriteNode!
+    var initialOrangeHexagonArea : SKSpriteNode!
+    var initialYellowCircleArea : SKSpriteNode!
+    var initialYellowSquareArea : SKSpriteNode!
+    var initialYellowHexagonArea : SKSpriteNode!
+    
+    
+    var firstBallon: SKSpriteNode!
+    var finalBaloon: SKSpriteNode!
+    
     var circleArea: SKSpriteNode!
     var squareArea: SKSpriteNode!
     var hexagonArea: SKSpriteNode!
     
+    var fullCircle = false
+    var fullSquare = false
+    var fullHexagon = false
+
     var color1 = false
     var color2 = false
     var color3 = false
     
     var background : SKSpriteNode!
+    var greenCharacter : SKSpriteNode!
     
     var selectedShape: SKSpriteNode?
     var musicPlayers: [NSSound] = []
@@ -62,12 +81,26 @@ public class GameScene: SKScene {
         yellowHexagon = (self.childNode(withName: "yellowHexagon") as! SKSpriteNode)
         yellowSquare = (self.childNode(withName: "yellowSquare") as! SKSpriteNode)
         
+        initialBlueCircleArea = (self.childNode(withName: "initialBlueCircleArea") as! SKSpriteNode)
+        initialBlueHexagonArea = (self.childNode(withName: "initialBlueHexagonArea") as! SKSpriteNode)
+        initialBlueSquareArea = (self.childNode(withName: "initialBlueSquareArea") as! SKSpriteNode)
+        initialOrangeCircleArea = (self.childNode(withName: "initialOrangeCircleArea") as! SKSpriteNode)
+        initialOrangeSquareArea = (self.childNode(withName: "initialOrangeSquareArea") as! SKSpriteNode)
+        initialOrangeHexagonArea = (self.childNode(withName: "initialOrangeHexagonArea") as! SKSpriteNode)
+        initialYellowCircleArea = (self.childNode(withName: "initialYellowCircleArea") as! SKSpriteNode)
+        initialYellowSquareArea = (self.childNode(withName: "initialYellowSquareArea") as! SKSpriteNode)
+        initialYellowHexagonArea = (self.childNode(withName: "initialYellowHexagonArea") as! SKSpriteNode)
+        
+        firstBallon = (self.childNode(withName: "firstBallon") as! SKSpriteNode)
+        finalBaloon = (self.childNode(withName: "finalBaloon") as! SKSpriteNode)
+        
         
         circleArea = (self.childNode(withName: "circleArea") as! SKSpriteNode)
         squareArea = ( self.childNode(withName: "squareArea") as! SKSpriteNode)
         hexagonArea = (self.childNode(withName: "hexagonArea") as! SKSpriteNode)
         
         background = (self.childNode(withName: "background2") as! SKSpriteNode)
+        greenCharacter = (self.childNode(withName: "greenCharacter") as! SKSpriteNode)
     }
     
     @objc static override public var supportsSecureCoding: Bool {
@@ -80,43 +113,60 @@ public class GameScene: SKScene {
     
     func touchDown(atPoint pos : CGPoint) { // funcao para arrastar as formas //
         if blueCircle.contains(pos){
+          
             selectedShape = blueCircle
             color1 = true
         }
         if blueHexagon.contains(pos){
+            
             selectedShape = blueHexagon
             color2 = true
         }
         if blueSquare.contains(pos){
+            
             selectedShape = blueSquare
             color3 = true
         }
         if orangeCircle.contains(pos) {
+            
             selectedShape = orangeCircle
             color1 = false
             
         }
         if orangeHexagon.contains(pos) {
+         
             selectedShape = orangeHexagon
             color2 = false
         }
         if orangeSquare.contains(pos) {
+           
             selectedShape = orangeSquare
             color3 = false
         }
         if yellowCircle.contains(pos) {
+           
             selectedShape = yellowCircle
             color1 = false
         }
         if yellowHexagon.contains(pos) {
+            
             selectedShape = yellowHexagon
             color2 = false
         }
         if yellowSquare.contains(pos) {
+            
             selectedShape = yellowSquare
             color3 = false
         }
-        
+        if circleArea.contains(pos){
+            fullCircle = false
+        }
+        if squareArea.contains(pos){
+            fullSquare = false
+        }
+        if hexagonArea.contains(pos){
+            fullHexagon = false
+        }
     }
     
     func touchMoved(toPoint pos : CGPoint) {
@@ -138,38 +188,74 @@ public class GameScene: SKScene {
         let playerIndex = musicNames.firstIndex(of: musicName)
         let player = musicPlayers [playerIndex!]
         
-        if selectedShape.name!.contains("Circle"){
+        if selectedShape.name!.contains("Circle")  {
+            
         
-            if circleArea.intersects(selectedShape){
+            if circleArea.contains(pos) && fullCircle == false {
                 selectedShape.position = circleArea.position
                 player.volume = 0.1
+                fullCircle = true
             }
             
             else {
+                if selectedShape.name!.contains("blue"){
+                    selectedShape.run(SKAction.move(to: initialBlueCircleArea.position, duration: 0.2))
+                    
+                } else if selectedShape.name!.contains("yellow"){
+                    selectedShape.run(SKAction.move(to: initialYellowCircleArea.position, duration: 0.2))
+                    
+                } else if selectedShape.name!.contains("orange"){
+                    selectedShape.run(SKAction.move(to: initialOrangeCircleArea.position, duration: 0.2))
+                }
                 player.volume = 0
+            
             }
 
         }
-        else if selectedShape.name!.contains("Square"){
+        else if selectedShape.name!.contains("Square") {
+            
         
-            if squareArea.intersects(selectedShape){
+            if squareArea.contains(pos) && fullSquare == false {
                 selectedShape.position = squareArea.position
                 player.volume = 0.1
+                fullSquare = true
             }
             
             else {
+                if selectedShape.name!.contains("blue"){
+                    selectedShape.run(SKAction.move(to: initialBlueSquareArea.position, duration: 0.2))
+                    
+                } else if selectedShape.name!.contains("yellow"){
+                    selectedShape.run(SKAction.move(to: initialYellowSquareArea.position, duration: 0.2))
+                    
+                } else if selectedShape.name!.contains("orange"){
+                    selectedShape.run(SKAction.move(to: initialOrangeSquareArea.position, duration: 0.2))
+                }
                 player.volume = 0
+                
             }
         }
-       else if selectedShape.name!.contains("Hexagon"){
+       else if selectedShape.name!.contains("Hexagon") {
+            
         
-            if hexagonArea.intersects(selectedShape){
+            if hexagonArea.contains(pos) && fullHexagon == false {
                 selectedShape.position = hexagonArea.position
                 player.volume = 0.1
+                fullHexagon = true
             }
             
             else {
+                if selectedShape.name!.contains("blue"){
+                    selectedShape.run(SKAction.move(to: initialBlueHexagonArea.position, duration: 0.2))
+                    
+                } else if selectedShape.name!.contains("yellow"){
+                    selectedShape.run(SKAction.move(to: initialYellowHexagonArea.position, duration: 0.2))
+                    
+                } else if selectedShape.name!.contains("orange"){
+                    selectedShape.run(SKAction.move(to: initialOrangeHexagonArea.position, duration: 0.2))
+                }
                 player.volume = 0
+               
             }
        }
        
@@ -188,7 +274,18 @@ public class GameScene: SKScene {
        }
         if color1 && color2 && color3 {
             background.texture = SKTexture (imageNamed: "backGroundBlue2")
+            greenCharacter.texture = SKTexture (imageNamed: "greenCharacter2")
+            firstBallon.alpha = 0
+            finalBaloon.alpha = 1
+            
         }
+        
+        if circleArea.position != blueCircle.position  || squareArea.position != blueSquare.position || hexagonArea != blueHexagon {
+            
+           // firstBallon.alpha = 1
+            
+        }
+        
         self.selectedShape = nil
     
     }
